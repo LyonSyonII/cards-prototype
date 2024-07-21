16d6c822314d2a166e8f76ea4d0e84dd7ebda911
lyonsyonii <liam@garriga.dev>
2024-07-21 12:35:44 +0200

    basic serve/connect example

--- src-astro/src-tauri/src/main.rs
+++ src-astro/src-tauri/src/main.rs
@@ -1,16 +1,95 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::{io::{Read, Write}, net::Ipv4Addr, str::FromStr};
+use tauri::{Emitter as _, Event, Listener as _, Window};
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![connect, serve])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn connect(window: Window, addr: &str) -> Result<(), String> {
+    let mut socket = std::net::TcpStream::connect(addr).map_err(err2str)?;
+    
+    let (tx, rx) = std::sync::mpsc::channel::<String>();
+    
+    let send_tx = tx.clone();
+    let send = window.listen("SEND", move |e| {
+        send_tx.send(e.payload().to_owned()).unwrap();
+    });
+    socket.set_nonblocking(true).map_err(err2str)?;
+    std::thread::spawn(move || {
+        let mut buf = Vec::with_capacity(u16::MAX as usize);
+        let err = loop {
+            if let Ok(s) = rx.try_recv() {
+                if let Err(e) = socket.write_all(s.as_bytes()) {
+                    break e.to_string();
+                }
+            } else if socket.read_to_end(&mut buf).is_ok() {
+                if let Err(e) = window.emit("RECEIVE", String::from_utf8_lossy(&buf)) {
+                    break e.to_string();
+                }
+            }
+        };
+        
+        println!("{err}");
+        window.unlisten(send);
+    });
+    
+    Ok(())
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn serve(window: Window) -> Result<String, String> {
+    let local_ip = local_ip_address::local_ip().unwrap();
+    let listener = std::net::TcpListener::bind("0.0.0.0:0").map_err(err2str)?;
+
+    let local_addr = {
+        let mut a = listener.local_addr().unwrap();
+        a.set_ip(local_ip);
+        a.to_string() 
+    };
+    println!("Listening on {local_addr}");
+    
+    std::thread::spawn(move || {
+        let (mut socket, addr) = listener.accept().unwrap();
+        println!("Accepted connection from {addr}");
+        window.emit("RECEIVE", addr.to_string()).unwrap();
+        
+        let (tx, rx) = std::sync::mpsc::channel::<String>();
+        let send_tx = tx.clone();
+        let send = window.listen("SEND", move |e| {
+            send_tx.send(e.payload().to_owned()).unwrap();
+        });
+        socket.set_nonblocking(true).unwrap();
+
+        let mut buf = Vec::with_capacity(u16::MAX as usize);
+        let err = loop {
+            if let Ok(s) = rx.try_recv() {
+                if let Err(e) = socket.write_all(s.as_bytes()) {
+                    break e.to_string();
+                }
+            } else if socket.read_to_end(&mut buf).is_ok() {
+                if let Err(e) = window.emit("RECEIVE", String::from_utf8_lossy(&buf)) {
+                    break e.to_string();
+                }
+            }
+        };
+        
+        println!("{err}");
+        window.unlisten(send);
+    });
+
+    Ok(local_addr)
+}
+
+fn err2str(err: impl std::error::Error) -> String {
+    err.to_string()
 }
--- src-astro/src/pages/index.ts
+++ src-astro/src/pages/index.ts
@@ -1,9 +1,37 @@
 import { invoke } from "@tauri-apps/api/core";
+import { listen, once } from "@tauri-apps/api/event";
 
-export function main() {
-  const greetInput = document.querySelector<HTMLInputElement>("#greet-in")!;
-  const greetOutput = document.querySelector<HTMLParagraphElement>("#greet-out")!;
-  document.querySelector<HTMLButtonElement>("button")!.addEventListener("click", async () => {
-    greetOutput.textContent = await invoke("greet", { name: greetInput.value });
+export async function main() {
+  await connect();
+  await serve();
+}
+
+async function connect() {
+  const output = document.querySelector<HTMLParagraphElement>("#output")!;
+  const input = document.querySelector<HTMLInputElement>("#connect-in")!;
+  const println = (s: string) => {
+    output.textContent += `[connect] ${s}\n`;
+  }
+
+  document.querySelector<HTMLButtonElement>("#connect-btn")!.addEventListener("click", async () => {
+    const addr = input.value;
+    await invoke("connect", { addr })
+      .then(() => println(`Connected with ${addr}`))
+      .catch(e => println(e));
+  });
+  // await listen<string>("RECEIVE", ({ payload }) => println(payload));
+}
+
+async function serve() {
+  const output = document.querySelector<HTMLParagraphElement>("#output")!;
+  const println = (s: string) => {
+    output.textContent += `[serve] ${s}\n`;
+  }
+
+  document.querySelector<HTMLButtonElement>("#serve-btn")!.addEventListener("click", async () => { 
+    invoke<string>("serve")
+      .then(addr => println(`Listening on ${addr}`))
+      .catch(e => println(e));
+    await once<string>("RECEIVE", ({ payload }) => println(`Connected with ${payload}`))
   });
 }
